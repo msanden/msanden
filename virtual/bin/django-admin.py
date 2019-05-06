93072726fb7f79edc33f2f4347eecda20ec9206a
Manuel Sande <mnlsande22@gmail.com>
2019-05-06 04:59:06 -0400

    portfolio landing page

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Sande/Documents/moringa-school-projects/moringa-core/django/portfolio/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
